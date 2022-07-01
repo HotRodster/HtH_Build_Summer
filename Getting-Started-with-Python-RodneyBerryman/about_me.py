dade1bf103962836a5dd60fc81d3be8bc9e3110f
Rodney Berryman <hotrodsterjr@gmail.com>
2022-06-30 22:13:49 -0700

    adding 'getting started with python' activity

---
+++ Getting-Started-with-Python-RodneyBerryman/about_me.py
@@ -0,0 +1,22 @@
+full_name = "Rodney Berryman"
+nickname = "Rodney"
+age = 20
+python_experience = True
+
+hobbies = ["making YouTube videoes", "watching anime", "video games", "reading manga","reading novels"]
+
+favorites = {
+  "movie": "Spider-Man: Into the Spiderverse",
+  "number": 169,
+  "food": "catfish",
+  "hobby": hobbies[0],
+  "anime": "One Piece",
+}
+
+print(full_name, nickname, age, python_experience)
+print()
+print(hobbies)
+print()
+print(favorites)
+
+all_vars = dict(vars())
