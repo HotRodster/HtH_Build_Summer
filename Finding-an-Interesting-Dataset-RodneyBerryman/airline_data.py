ae414c4de9b276cbeb51607799f193e824ef7132
HotRodster <hotrodsterjr@gmail.com>
2022-07-19 14:46:30 -0700

    new dataset

--- Finding-an-Interesting-Dataset-RodneyBerryman/airline_data.py
+++
@@ -1,9 +0,0 @@
-import pandas
-
-pandas.options.display.max_columns = None
-pandas.options.display.max_rows = None
-
-airlines_data = pandas.read_csv(
-    "Finding-an-Interesting-Dataset-RodneyBerryman/Airlines.csv")
-
-print(airlines_data)
